b2127ceb5419f7c9f266b46303344f0166f2261d
habibasaker <habiba.amr.osama@gmail.com>
2026-05-05 00:47:27 +0200

    Update User.h

--- include /User.h
+++ include /User.h
@@ -1,1 +1,25 @@
+#ifndef USER_H
+#define USER_H
 
+#include <string>
+
+class User {
+protected:
+    int id;
+    std::string name;
+    std::string email;
+
+public:
+    User();
+    User(int id, std::string name, std::string email);
+
+    virtual void displayInfo() const;
+
+    int getId() const;
+    std::string getName() const;
+    std::string getEmail() const;
+
+    virtual ~User();
+};
+
+#endif
