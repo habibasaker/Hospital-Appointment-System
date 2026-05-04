180a80268cb431b661ea3058f2e2e57f5a2c0770
habibasaker <habiba.amr.osama@gmail.com>
2026-05-05 00:43:55 +0200

    Update User.cpp

--- src/User.cpp
+++ src/User.cpp
@@ -1,1 +1,25 @@
+#include "User.h"
+#include <iostream>
 
+User::User() : id(0), name(""), email("") {}
+
+User::User(int id, std::string name, std::string email)
+    : id(id), name(name), email(email) {}
+
+void User::displayInfo() const {
+    std::cout << "ID: " << id << "\nName: " << name << "\nEmail: " << email << std::endl;
+}
+
+int User::getId() const {
+    return id;
+}
+
+std::string User::getName() const {
+    return name;
+}
+
+std::string User::getEmail() const {
+    return email;
+}
+
+User::~User() {}
