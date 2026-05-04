1bdf04cd4e3ae02bfdc46a1e03b926195ccf1773
habibasaker <habiba.amr.osama@gmail.com>
2026-05-05 00:53:46 +0200

    Add unit test for Patient's addAppointment method

--- tests/test_patient.cpp
+++ tests/test_patient.cpp
@@ -1,1 +1,16 @@
 
+#include <gtest/gtest.h>
+#include "Patient.h"
+
+TEST(PatientTest, AddAppointment) {
+    Patient p(1, "Sara", "sara@mail.com");
+
+    p.addAppointment(100);
+    p.addAppointment(200);
+
+    std::vector<int> apps = p.getAppointments();
+
+    ASSERT_EQ(apps.size(), 2);
+    EXPECT_EQ(apps[0], 100);
+    EXPECT_EQ(apps[1], 200);
+}
