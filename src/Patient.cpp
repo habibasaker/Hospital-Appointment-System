a05e8418e47bf949093fbef3d3de741ae9978437
habibasaker <habiba.amr.osama@gmail.com>
2026-05-05 00:44:14 +0200

    Update Patient.cpp

--- src/Patient.cpp
+++ src/Patient.cpp
@@ -1,1 +1,26 @@
+#include "Patient.h"
+#include <iostream>
 
+Patient::Patient() : User() {}
+
+Patient::Patient(int id, std::string name, std::string email)
+    : User(id, name, email) {}
+
+void Patient::addAppointment(int appointmentId) {
+    appointmentIds.push_back(appointmentId);
+}
+
+void Patient::displayInfo() const {
+    std::cout << "Patient Info:\n";
+    User::displayInfo();
+
+    std::cout << "Appointments: ";
+    for (int id : appointmentIds) {
+        std::cout << id << " ";
+    }
+    std::cout << std::endl;
+}
+
+std::vector<int> Patient::getAppointments() const {
+    return appointmentIds;
+}
