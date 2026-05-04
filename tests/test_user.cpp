10cc680d9cffbb8c45264f36dd386ddd98cef4e6
habibasaker <habiba.amr.osama@gmail.com>
2026-05-05 00:53:23 +0200

    Add unit tests for User class
    Added unit tests for User class constructor and getters.

--- tests/test_user.cpp
+++ tests/test_user.cpp
@@ -1,1 +1,10 @@
+#include <gtest/gtest.h>
+#include "User.h"
 
+TEST(UserTest, ConstructorAndGetters) {
+    User u(1, "Ali", "ali@mail.com");
+
+    EXPECT_EQ(u.getId(), 1);
+    EXPECT_EQ(u.getName(), "Ali");
+    EXPECT_EQ(u.getEmail(), "ali@mail.com");
+}
