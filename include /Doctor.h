5c25b2291099299fc8bce2899bda009352e3413c
habibasaker <habiba.amr.osama@gmail.com>
2026-05-05 00:49:28 +0200

    Add Doctor class definition with constructors and methods

--- include /Doctor.h
+++ include /Doctor.h
@@ -1,1 +1,22 @@
+#ifndef DOCTOR_H
+#define DOCTOR_H
+
+#include "User.h"
+#include <string>
+
+class Doctor : public User {
+private:
+    std::string specialty;
+
+public:
+    Doctor();
+    Doctor(int id, std::string name, std::string email, std::string specialty);
+
+    void displayInfo() const override;
+
+    std::string getSpecialty() const;
+};
+
+#endif
+
 
