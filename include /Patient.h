317d5b5d190c14e0c88855aed4d57f9e9b66a92b
habibasaker <habiba.amr.osama@gmail.com>
2026-05-05 00:48:26 +0200

    Create Patient class for managing appointments
    Added Patient class inheriting from User with appointment management.

--- include /Patient.h
+++ include /Patient.h
@@ -1,1 +1,21 @@
+#ifndef PATIENT_H
+#define PATIENT_H
 
+#include "User.h"
+#include <vector>
+
+class Patient : public User {
+private:
+    std::vector<int> appointmentIds;
+
+public:
+    Patient();
+    Patient(int id, std::string name, std::string email);
+
+    void addAppointment(int appointmentId);
+    void displayInfo() const override;
+
+    std::vector<int> getAppointments() const;
+};
+
+#endif
