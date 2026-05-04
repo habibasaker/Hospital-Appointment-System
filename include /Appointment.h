0d9a6920da2e26d07ae00347cf615a1585f391d9
habibasaker <habiba.amr.osama@gmail.com>
2026-05-05 00:50:06 +0200

    Update Appointment.h

--- include /Appointment.h
+++ include /Appointment.h
@@ -1,1 +1,22 @@
+#ifndef APPOINTMENT_H
+#define APPOINTMENT_H
 
+#include <string>
+
+class Appointment {
+private:
+    int appointmentId;
+    int patientId;
+    int doctorId;
+    std::string date;
+
+public:
+    Appointment();
+    Appointment(int appId, int patientId, int doctorId, std::string date);
+
+    void display() const;
+
+    int getAppointmentId() const;
+};
+
+#endif
