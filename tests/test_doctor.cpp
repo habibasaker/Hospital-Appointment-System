2a951e27ab77be443264e6bcae054e3d8c7b7257
habibasaker <habiba.amr.osama@gmail.com>
2026-05-05 00:54:12 +0200

    Add unit test for Doctor class constructor

--- tests/test_doctor.cpp
+++ tests/test_doctor.cpp
@@ -1,1 +1,10 @@
+#include <gtest/gtest.h>
+#include "Doctor.h"
 
+TEST(DoctorTest, ConstructorAndSpecialty) {
+    Doctor d(10, "Dr. Omar", "omar@mail.com", "Cardiology");
+
+    EXPECT_EQ(d.getId(), 10);
+    EXPECT_EQ(d.getName(), "Dr. Omar");
+    EXPECT_EQ(d.getSpecialty(), "Cardiology");
+}
